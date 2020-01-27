967742534eeb86d051c68620dfbbde24e9e5a5ac
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-01-26 17:40:46 -0800

    Customized prompt and moved to vim bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,30 @@
 
+fish_vi_key_bindings
+
 alias config='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+abbr run 'bash -ci'
+abbr bash 'bash -i'
+
+function fish_prompt --description 'Write out the prompt'
+	set -l color_cwd
+    set -l suffix
+    switch "$USER"
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
+            set color_cwd $fish_color_cwd
+            set suffix '>'
+    end
+
+    set time (date +"%R")
+    echo -n -s "$time $USER" @ (prompt_hostname) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+end
+
+function fish_right_prompt
+    # Intentionally blank
+end
