da38070dc992d3ae418d1c596dc379efcc618cca
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-02-13 08:33:42 -0800

    Do not reclone the git repo for polybar

--- self_compile/build/polybar.sh
+++ self_compile/build/polybar.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-git clone https://github.com/polybar/polybar.git
-cd polybar || exit
+if [ ! -d polybar ]; then
+    git clone https://github.com/polybar/polybar.git
+    cd polybar || exit
+else
+    cd polybar || exit
+    git pull
+fi
+
 ./build.sh -j -n -p -A
