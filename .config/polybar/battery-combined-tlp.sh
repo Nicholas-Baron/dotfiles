f34125a19b56ca6a57c7aec310952d6b97a300e6
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-04-28 11:57:23 -0700

    Use hostname to check if on desktop

--- .config/polybar/battery-combined-tlp.sh
+++ .config/polybar/battery-combined-tlp.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+if [ $(hostname) = "archbox" ]; then
+    echo "On desktop"
+    exit
+fi
+
 battery=$(sudo tlp-stat -b | tac | grep -m 1 "Charge" | tr -d -c "[:digit:],.")
 charging=$(sudo tlp-stat -b | tac | grep -m 1 "status" | grep -Eio "(discharging|charging)" | tr '[A-Z]' '[a-z]')
 
 if [ $battery = "" ] || [ $charging = "" ]; then
-    echo "On desktop?"
+    echo "Is TLP installed?"
 else
     echo "$charging $battery %"
 fi
