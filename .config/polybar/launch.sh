219790f2b52ebdaf95fd1dc338d41b5c6f7f0ffc
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2022-12-29 19:54:14 -0800

    Put status bars on all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,8 +4,11 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "---" | tee -a /tmp/polybar.log
-polybar -r mybar >>/tmp/polybar.log 2>&1 &
-
-echo "Bar launched"
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -r mybar &
+    done
+else
+    polybar -r mybar &
+fi
 
