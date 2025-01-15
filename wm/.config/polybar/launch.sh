93547603cdcf249d85a22f3762b7260b5d6b7874
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2025-01-15 10:41:43 -0800

    Save logs from polybar

--- wm/.config/polybar/launch.sh
+++ wm/.config/polybar/launch.sh
@@ -6,9 +6,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar -r mybar &
+        MONITOR=$m polybar -r mybar 2> .config/polybar/log_$m &
     done
 else
-    polybar -r mybar &
+    polybar -r mybar 2> .config/polybar/log &
 fi
 
