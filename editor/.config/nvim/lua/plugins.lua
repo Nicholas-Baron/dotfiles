cf00b00093e170de1f846ec554ba1362fc560a0e
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2025-09-09 21:58:46 -0700

    Formatting pass

--- editor/.config/nvim/lua/plugins.lua
+++ editor/.config/nvim/lua/plugins.lua
@@ -97,8 +97,10 @@ local plugins = {
         'neovim/nvim-lspconfig',
         config = lsp_config
     },
-    { "ellisonleao/gruvbox.nvim", priority = 1000 ,
-        config = function() 
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
             vim.o.background = "dark"
             vim.cmd([[colorscheme gruvbox]])
         end
